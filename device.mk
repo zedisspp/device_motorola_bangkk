5df07a00bd2b09c2f18b680f83c3320750cface9
kenway214 <kenway214@outlook.com>
2026-07-18 19:37:40 -0300

    bangkk: Add GameBar Performance Overlay
    Signed-off-by: zedisspp <juniordesigner077@gmail.com>

--- device.mk
+++ device.mk
@@ -105,3 +105,6 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/bangkk/bangkk-vendor.mk)
+
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
