7db0c0414f5f4e3dea9e2665a2286c6f9bbd6125
zedisspp <juniordesigner077@gmail.com>
2026-08-19 15:31:18 -0300

    Revert "bangkk: Bringup EvolutionX"
    This reverts commit c0908d1e145324c9281f94f18907ac21fa75b0bf.

--- lineage_bangkk.mk
+++ lineage_bangkk.mk
@@ -26,10 +26,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="bangkk_g-user 15 V1TCS35H.88-16-4 d201a-da14f release-keys MV-301" \
     BuildFingerprint=motorola/bangkk_g/bangkk:15/V1TCS35H.88-16-4/d201a-da14f:user/release-keys \
     DeviceProduct=bangkk_g
-
-WITH_GMS := true
-BUILD_BCR := true
-TARGET_HAS_UDFPS := true
-TARGET_INCLUDE_BOOT_ANIMATIONS := true
-TARGET_DISABLE_EPPE := true
-TARGET_ENABLE_BLUR := true
