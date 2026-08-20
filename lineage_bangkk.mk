0548c3414f5c936d0fb660209767198b3ddc39a0
ZedissP <juniordesigner077@gmail.com>
2026-08-20 16:56:09 -0300

    bangkk: Drop bringup Nyx
    Removing the product and vendor fields that I forgot in the previous commit.

--- lineage_bangkk.mk
+++ lineage_bangkk.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/bangkk/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := nyx_bangkk
+PRODUCT_NAME := lineage_bangkk
 PRODUCT_DEVICE := bangkk
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
