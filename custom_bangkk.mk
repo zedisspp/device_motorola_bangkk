c3c39055299ef20f7686708a36a57ca8c87dbc66
ZedissP <juniordesigner077@gmail.com>
2026-09-02 23:10:24 -0300

    bangkk: Initial Bringup Pixel

--- custom_bangkk.mk
+++ custom_bangkk.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bangkk device
 $(call inherit-product, device/motorola/bangkk/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixel stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_bangkk
+PRODUCT_NAME := custom_bangkk
 PRODUCT_DEVICE := bangkk
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
@@ -26,8 +26,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="bangkk_g-user 15 V1TCS35H.88-16-4 d201a-da14f release-keys MV-301" \
     BuildFingerprint=motorola/bangkk_g/bangkk:15/V1TCS35H.88-16-4/d201a-da14f:user/release-keys \
     DeviceProduct=bangkk_g
-
-# Asta flags
-LINEAGE_BUILDTYPE := OFFICIAL
-WITH_GMS := true
-USE_PICO := true
