c667d3ec4da51a1f42903910e3e7d2e5bdb3e4c5
zedisspp <juniordesigner077@gmail.com>
2026-09-17 11:06:42 -0300

    Revert "updater: Setup OTA for PixelOS"
    This reverts commit 950300c7fe7b161e850d1af5b1b1e8640293a04c.

--- device.mk
+++ device.mk
@@ -29,8 +29,7 @@ PRODUCT_PACKAGES += \
     SettingsResDevice \
     SystemUIResDevice \
     WifiResDevice \
-    PixelUpdater \
-    
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
