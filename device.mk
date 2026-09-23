bdf5e002b2e58f71172048d2f4438ea8f0dbd69a
zedisspp <juniordesigner077@gmail.com>
2026-09-23 12:55:26 -0300

    audio: Add Spatializer output policy

--- device.mk
+++ device.mk
@@ -33,7 +33,11 @@ PRODUCT_PACKAGES += \
     SystemUIResDevice \
     WifiResDevice \
     PixelUpdater \
-    
+
+# Spatial Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.spatializer_enabled=true
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
